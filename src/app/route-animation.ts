637b000b2c0079003388650f75579ab5366a7ebc
Justin Clark <justinclark.dev@gmail.com>
2019-05-24 16:04:37 -0700

    Refactors Angular animations meta data.

--- src/app/route-animation.ts
+++ src/app/route-animation.ts
@@ -8,58 +8,45 @@ import {
   state
 } from '@angular/animations';
 
-export const slideInMenu =
-trigger('openClose', [
-  state('open', style({
-    opacity: 1,
-    transform: 'translateX(0%)'
-  })),
-  state('closed', style({
-    opacity: 0.5,
-    transform: 'translateX(-100%)'
-  })),
-  transition('open => closed', [
-    animate('0.5s')
-  ]),
-  transition('closed => open', [
-    animate('0.5s')
-  ])
-]);
+const animationState: any = {
+  sliderState: [
+    state('open', style({
+      opacity: 1,
+      transform: 'translateX(0%)'
+    })),
+    state('closed', style({
+      opacity: 0.5,
+      transform: 'translateX(-100%)'
+    })),
+    transition('open => closed', [
+      animate('0.15s')
+    ]),
+    transition('closed => open', [
+      animate('0.15s')
+    ])
+  ]
+};
 
-export const slideInAnimation =
+export const slideInMenu: any =
+  trigger('openClose', animationState.sliderState);
+
+export const slideInAnimation: any =
   trigger('routeAnimations', [
-
-      // TODO: refactor the below to use the 'openClose' trigger within 'slideInMenu', calling it from this location
-      state('open', style({
-        opacity: 1,
-        transform: 'translateX(0%)'
-      })),
-      state('closed', style({
-        opacity: 0.5,
-        transform: 'translateX(-100%)'
-      })),
-      transition('open => closed', [
-        animate('0.5s')
-      ]),
-      transition('closed => open', [
-        animate('0.5s')
-      ]),
-
-      transition('* <=> *', [
-          query(':enter, :leave',
-                style({ position: 'fixed', width: '100%' }),
-                { optional: true }),
-          group([
-                query(':enter',[
-                    style({ transform: 'translateX(100%)' }),
-                    animate('0.5s ease-in-out',
-                    style({ transform: 'translateX(0%)' }))
-                ], { optional: true }),
-                query(':leave', [
-                    style({ transform:   'translateX(0%)'}),
-                    animate('0.5s ease-in-out',
-                    style({ transform: 'translateX(-100%)' }))
-                ], { optional: true }),
-          ])
+    transition('* <=> *', [
+      query(':enter, :leave',
+        style({ position: 'fixed', width: '100%' }),
+        { optional: true }),
+      group([
+        query(':enter', [
+          style({ transform: 'translateX(100%)' }),
+          animate('0.3s ease',
+          style({ transform: 'translateX(0%)' }))
+        ], { optional: true }),
+        query(':leave', [
+          style({ transform:   'translateX(0%)'}),
+          animate('0.3s ease',
+          style({ transform: 'translateX(-100%)' }))
+        ], { optional: true }),
       ])
-]);
+    ])
+  ]);
